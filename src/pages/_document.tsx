84c656d2aa2ebe7137840a99f18ad16d4affba31
ankushKun <ankush4singh@gmail.com>
2024-06-05 14:32:56 +0530

    icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head >
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" href="/icon.svg" type="image/svg" />
-        <link rel="apple-touch-icon" href="/apple-icon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/icon.png" type="image/png" />
       </Head>
       <body>
         <Main />
